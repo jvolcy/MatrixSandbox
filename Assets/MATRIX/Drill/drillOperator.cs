58bc1180d36ce518e4b0f2ce3565739613fd38f4
jvolcy <jerry.volcy@gmail.com>
2025-09-19 16:32:50 -0400

    Added drill and table

---
+++ Assets/MATRIX/Drill/drillOperator.cs
@@ -0,0 +1,22 @@
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class drillOperator : MonoBehaviour
+{
+    [SerializeField] Animator buttonAnimator;
+    
+    [SerializeField] Animator bitAnimation;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float drillSpeed = buttonAnimator.GetLayerWeight(1);
+        bitAnimation.speed = drillSpeed;
+    }
+}
